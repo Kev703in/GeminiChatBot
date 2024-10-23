8882f5ba3d112f2e24d3604b514dbe3a60e3f91a
Kevin Richard <kev703in@gmail.com>
2024-10-22 19:33:00 -0700

    env variable start with VITE

--- src/config/gemini.js
+++ src/config/gemini.js
@@ -4,7 +4,8 @@ import {
     HarmBlockThreshold,
   } from "@google/generative-ai";
   
-  const apiKey = import.meta.env.REACT_APP_GOOGLE_API_KEY;
+  // const apiKey = "AIzaSyDb4-Fb0JDko0lys_7TzAMfPp_Me8xBCt0"
+  const apiKey = import.meta.env.VITE_APP_GOOGLE_API_KEY;
   const genAI = new GoogleGenerativeAI(apiKey);
   
   const model = genAI.getGenerativeModel({
